df6ff6b8b7d79540225bd77f433203f7f3503741
Alex-Messi <33alexmuha2007@gmail.com>
2023-07-25 15:57:29 +0300

    main3

---
+++ math.js
@@ -0,0 +1,21 @@
+function sum(num1, num2){
+    return num1 + num2;
+}
+function minus(num1, num2) {
+    return num1 - num2;
+}
+function multiplication(num1, num2) {
+    return num1 * num2;
+}
+function divison(num1, num2) {
+    return num1 / num2;
+}
+function degree(num1, num2) {
+    return num1 ** num2;
+}
+function percent(num1, num2) {
+    return num1 % num2;
+}
+function root(num1, num2) {
+    return num1 ** (1/num2);
+}
---
+++ megagigacalculator.js
@@ -0,0 +1,48 @@
+const OPERATIONS = {
+    sum: '+',
+    minus: '-',
+    multiplication: '*',
+    divison: '/',
+    degree: '**',
+    percent: '%',
+    root: 'sq',
+};
+
+function calculate({ num1, num2, operetion }){
+    let result = null;
+
+    switch (operation) {
+        case OPERATIONS.sum:
+            result = sum(num1,num2)
+            break;
+
+        case OPERATIONS.minus:
+            result = minus(num1,num2)
+            break;
+
+        case OPERATIONS.multiplication:
+            result = multiplication(num1,num2)
+            break;
+
+        case OPERATIONS.divison:
+            result = divison(num1,num2)
+            break;
+
+        case OPERATIONS.degree:
+            result = degree(num1,num2)
+            break;
+
+        case OPERATIONS.percent:
+            result = percent(num1,num2)
+            break;
+
+        case OPERATIONS.root:
+            result = root(num1,num2)
+            break;
+
+
+    }
+
+    result result;
+}
+
